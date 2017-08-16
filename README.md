92de938da8c20d10c1d587359f8ca3392b2d78a8
Karan Joshi <karanjoshi31@live.com>
2017-08-15 20:17:40 -0500

    edited readme.md file and initial push

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# System-Info-App
+<h1>System-Info-App</h1>
+
+<h4>Objectives:</h4>
+<ul>
+  <li>View you system information</li>
+  <li>Explore the ElectronJS library</li>
+</ul>
+
+<h4>Technologies Used:</h4>
+
+<ul>
+  <li>HTML</li>
+  <li>CSS</li>
+  <li>javascript</li>
+  <li>Electron.JS</li>
+  <li>Node.JS</li>
+</ul>
+
+<h4>Start Instructions:</h4>
+
+  `Type the following command inside your terminal:`</br>
+  `npm start`
---
+++ main.js
@@ -0,0 +1,39 @@
+const {app, BrowserWindow} = require('electron');
+
+const path = require('path');
+
+const url = require('url');
+
+//init win
+let win;
+
+function createWindow() {
+  //create browser window
+  win = new BrowserWindow({width:800,height:600, icon:__dirname+'/img/ComputerCircle.ico'});
+
+  //load index.html
+  win.loadURL(url.format({
+    pathname: path.join(__dirname,'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+
+  //Open devtools
+  //win.webContents.openDevTools();
+
+  win.on('closed', ()=>{
+    win = null;
+  });
+}
+
+//run create window function
+app.on('ready', createWindow);
+
+
+//quit when all windows are closed
+
+app.on('windo-all-closed', ()=>{
+  if(process.platform !== 'darwin'){
+    app.quite();
+  }
+});
